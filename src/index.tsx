31f2962f8ae1dae887b681b1d43d5805f43e2dc6
Your Name <you@example.com>
2020-05-11 10:22:43 +0200

    App.tsx - cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -4,29 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-
 // ;(window).React = React
 // ;(window).ReactDOM = ReactDOM
 
-type P = {
-  color: string, person: string, pet: string
-}
-
-const PersonCard: React.FunctionComponent<P> = (props) => <div className="test" style={{ color: props.color }}>
-  <p>{props.person} has a  {props.pet}</p>
-</div>
-
-
 
 ReactDOM.render(
-  <div>
-    <PersonCard color={'red'} person={'ALice'} pet={'Cat'} />
-    <PersonCard color={'green'} person={'Bob'} pet={'Dog'} />
-    <PersonCard color={'blue'} person={'Cat'} pet={'Cat'} />
-  </div>,
-  //   <React.StrictMode>
-  //     <App />
-  //   </React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
