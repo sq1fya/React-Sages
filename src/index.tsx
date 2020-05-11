3a8c6d62e5e4239d59afa3db8d5af5362efbabf6
Your Name <you@example.com>
2020-05-11 10:17:19 +0200

    JSX - JS + XML, React.createElement, REactDOM.render

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,26 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+// ;(window).React = React
+// ;(window).ReactDOM = ReactDOM
+
+const PersonCard =  (dane: any) => {
+  return <div className="test" style={{ color: dane.color }}>
+    <p>{dane.person} has a  {dane.pet}</p>
+  </div>
+}
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <PersonCard color={'red'} person={'ALice'} pet={'Cat'} />
+    <PersonCard color={'green'} person={'Bob'} pet={'Dog'} />
+    <PersonCard color={'blue'} person={'Cat'} pet={'Cat'} />
+  </div>,
+  //   <React.StrictMode>
+  //     <App />
+  //   </React.StrictMode>,
   document.getElementById('root')
 );
 
